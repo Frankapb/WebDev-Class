f2bbcd543d1b5ff4bde3110f2b6875bd526deedf
Frankapb <fapb7891@gmail.com>
2021-05-06 18:40:00 +0200

    update JS exercise

--- exercise-js/assignment.js
+++ exercise-js/assignment.js
@@ -35,4 +35,4 @@ details:[
 console.log("Hi my name is "+ curriculumVitae[0].details[0]+" "+curriculumVitae[0].details[1]
 +". I am from "+ curriculumVitae[0].details[3]+" and I currently live in "+curriculumVitae[0].details[2]+" "+curriculumVitae[0].details[4]+
 ". I`ve recently graduated as "+curriculumVitae[0].details[5]+" and complete my studies in "+ curriculumVitae[1].details[0] + " in "+curriculumVitae[1].details[1]+
-". The location of "+curriculumVitae[2].details[0] + " is in "+curriculumVitae[2].details[1]);
+". The location of "+curriculumVitae[2].details[0] + " is in "+curriculumVitae[2].details[1]+".");
